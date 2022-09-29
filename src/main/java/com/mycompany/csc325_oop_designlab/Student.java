9ebf42b513d9dfc5521b73f90209ce7cf1168e93
MurilloEdson <>
2022-09-29 10:04:32 -0400

    First Todo has been completed

---
+++ src/main/java/com/mycompany/csc325_oop_designlab/Student.java
@@ -0,0 +1,17 @@
+package com.mycompany.csc325_oop_designlab;
+
+/**
+ *
+ * @author MoaathAlrajab
+ */
+
+public class Student extends Human{
+
+	
+	// ToDo 2: Fix the resulting errors
+	
+	// ToDo 3: Add a field for GPA and create setter and getter
+	
+	// ToDo 4: Add comments to your code
+
+}
