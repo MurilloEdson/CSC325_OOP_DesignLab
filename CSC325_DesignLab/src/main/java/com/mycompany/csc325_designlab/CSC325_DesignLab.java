db8f06c32b9a8460d40d363adc12ab76590b2e66
MurilloEdson <>
2022-09-30 19:42:58 -0400

    Ninth and tenth todo have already been done

--- CSC325_DesignLab/src/main/java/com/mycompany/csc325_designlab/CSC325_DesignLab.java
+++ CSC325_DesignLab/src/main/java/com/mycompany/csc325_designlab/CSC325_DesignLab.java
@@ -9,17 +9,16 @@ public class CSC325_DesignLab {
     public static void main(String[] args) {
 
         // ToDo 8: The senior class should have a minimum of 85 credits  
-        // ToDo 9: Add a toString method for Freshman class
-        // ToDo 10: Add a toString method for Senior class
+
         Student std1 = new Freshman("James", 20, 12); // name, age, credits
 
         Student std2 = new Senior("John", 30, 90);
 
         // ToDo 11: Set the gpa of the student using the scanner and user
         // 			input and then print the output.
-        System.out.println(std1);
+        System.out.println(std1.toString());
 
-        System.out.println(std2);
+        System.out.println(std2.toString());
 
         // ToDo 12: add comments and explain your code
         // ToDo 13: submit using a pull request.
