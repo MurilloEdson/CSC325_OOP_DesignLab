9402dbd07d386bd5dd0828d7cb0693028a9983f6
MurilloEdson <>
2022-09-30 21:15:14 -0400

    Eighth todo has been completed

--- CSC325_DesignLab/src/main/java/com/mycompany/csc325_designlab/CSC325_DesignLab.java
+++ CSC325_DesignLab/src/main/java/com/mycompany/csc325_designlab/CSC325_DesignLab.java
@@ -11,23 +11,28 @@ public class CSC325_DesignLab {
 
     public static void main(String[] args) {
 
-        // ToDo 8: The senior class should have a minimum of 85 credits  
 
         Student std1 = new Freshman("James", 20, 12); // name, age, credits
 
-        Student std2 = new Senior("John", 30, 90);
+        Student std2 = new Senior("John", 30, 86);
         
         Scanner scnr = new Scanner(System.in);
-        System.out.println("Please input new credits");
+        System.out.println("Please input new GPA");
         
-        int newGPA = scnr.nextInt();
+        double newGPA = scnr.nextDouble();
         
-        std1.setCredits(newGPA);
-        System.out.println(std1.getName() + "'s new gpa is "+std1.getCredits());
+        std1.setGPA(newGPA);
+        System.out.println(std1.getName() + "'s new gpa is "+std1.getGPA());
 
         System.out.println(std1.toString());
 
         System.out.println(std2.toString());
+        
+        if(std2.passing){
+            System.out.println(std2.getName() +" is passing");
+        }else{
+            System.out.println(std2.getName() +" is NOT passing");
+        }
 
         // ToDo 12: add comments and explain your code
         // ToDo 13: submit using a pull request.
