31dd3f608656b292e3a005759ce46911100f088b
MurilloEdson <>
2022-09-30 22:56:05 -0400

    The final commit

--- CSC325_DesignLab/src/main/java/com/mycompany/csc325_designlab/CSC325_DesignLab.java
+++ CSC325_DesignLab/src/main/java/com/mycompany/csc325_designlab/CSC325_DesignLab.java
@@ -10,31 +10,29 @@ import java.util.Scanner;
 public class CSC325_DesignLab {
 
     public static void main(String[] args) {
-
-
+        
+        //New students have been created
         Student std1 = new Freshman("James", 20, 12); // name, age, credits
-
         Student std2 = new Senior("John", 30, 86);
         
+        //GPA is set for one student
         Scanner scnr = new Scanner(System.in);
         System.out.println("Please input new GPA");
-        
         double newGPA = scnr.nextDouble();
-        
         std1.setGPA(newGPA);
         System.out.println(std1.getName() + "'s new gpa is "+std1.getGPA());
 
+        //toString is practiced and information is displayed
         System.out.println(std1.toString());
-
         System.out.println(std2.toString());
         
+        //Test to see is senior is passing with 85 credits
         if(std2.passing){
             System.out.println(std2.getName() +" is passing");
         }else{
             System.out.println(std2.getName() +" is NOT passing");
         }
 
-        // ToDo 12: add comments and explain your code
         // ToDo 13: submit using a pull request.
     }
 }
